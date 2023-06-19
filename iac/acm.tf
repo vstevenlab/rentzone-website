5e59a65442c1b53734f62f00da8c7a4343653eae
vstevenlab <labsteven19@gmail.com>
2023-06-18 20:53:42 -0400

    Create Certificate

--- iac/acm.tf
+++ iac/acm.tf
@@ -38,3 +38,5 @@ resource "aws_acm_certificate_validation" "acm_certificate_validation" {
   certificate_arn         = aws_acm_certificate.acm_certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.route53_record : record.fqdn]
 }
+
+
